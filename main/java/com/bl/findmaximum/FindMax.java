0c2a1bf420eb207d1d579b7cec1cbddc66fdf184
rajeshpal652 <rajesh.pal.652@.com>
2022-04-29 00:23:33 +0530

    Added: Find Maximum program using generic method

--- main/java/com/bl/findmaximum/FindMax.java
+++ main/java/com/bl/findmaximum/FindMax.java
@@ -1,11 +1,20 @@
 package com.bl.findmaximum;
 /*
 @Author: Rajesh Pal
-Finding the maximum among the three given strings and running test cases for that.
+Extending the max method to take more than three parameters.
  */
-public class FindMax {
-    public String findMaximum(String x , String y , String z){
-        String max = x;
+public class FindMax <T extends Comparable<T>> {
+    T x , y , z ;
+    public FindMax(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public T testMaximum(){
+        return   FindMax.testMaximum(x , y , z);
+    }
+    public static  <T extends Comparable<T>> T testMaximum(T x , T y , T z){
+        T max =  x;
         if(y.compareTo(max)>0){
             max=y;
         }
--- test/java/com/bl/findmaximum/TestFindMax.java
+++ test/java/com/bl/findmaximum/TestFindMax.java
@@ -5,21 +5,28 @@ import org.junit.Test;
 
 public class TestFindMax {
     @Test
-    public void findMaxFromGivenThreeString(){
-        FindMax findMax = new FindMax();
-        String max = findMax.findMaximum("abcdef","abcde","abcd");
-        Assert.assertEquals(("abcdef"),max);
+    public void findMaxFromGivenThreeIntegers(){
+        FindMax<Integer> findMax = new <Integer>FindMax(9,10,11);
+        int max = findMax.testMaximum(10,11,12);
+        Assert.assertEquals(12,max);
     }
     @Test
-    public void findMaxFromGivenThreeStringWhereMaxIsAtPositionTwo(){
-        FindMax findMax = new FindMax();
-        String max = findMax.findMaximum("abcd","abcdef","abc");
-        Assert.assertEquals(("abcdef"),max);
+    public void findMaxFromGivenThreeFloatWhereMaxIsAtPositionTwo(){
+        FindMax<Float> findMax = new<Float> FindMax(9.5f,11.6f,8.8f);
+        Float max = findMax.testMaximum(9.5f,11.6f,8.8f);
+        Assert.assertEquals(11.6f,max,0.1);
     }
     @Test
     public void findMaxFromGivenThreeStringWhereMaxIsAtPositionThree(){
-        FindMax findMax = new FindMax();
-        String max = findMax.findMaximum("abc","abcde","abcdefg");
-        Assert.assertEquals("abcdefg",max);
+        FindMax<String> findMax = new <String>FindMax("apple","peaches","bananaaa");
+        String  max = findMax.testMaximum("abcde","abcd","ab");
+        Assert.assertEquals("abcde",max);
     }
+    @Test
+    public void findMaxFromGivenThreeGenericsFloatWhereMaxIsAtPositionTwo(){
+        FindMax<Float> findMax = new<Float> FindMax(12.8f,11.6f,8.8f);
+        Float max = findMax.testMaximum();
+        Assert.assertEquals(12.8f,max,0.1);
+    }
+
 }
